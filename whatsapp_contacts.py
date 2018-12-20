2e03b7941c47c24063481cc2c1355841fdac32ea
Dylan <dy199869@gmail.com>
2018-12-20 10:34:22 -0300

    leeo todos los contactos

--- whatsapp_contacts.py
+++ whatsapp_contacts.py
@@ -19,41 +19,67 @@ group_title = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div
 
 time.sleep(5)
 contactos = list([])
-for person in driver.find_elements_by_class_name('_2wP_Y'):
-    title = person.find_element_by_xpath('div/div/div[2]/div[1]/div[1]/span').text
-    # company = person.find_element_by_xpath('.//div[@class="company"]/a').text
-    contactos.append(title)
 
 
+def leer():
+    for person in driver.find_elements_by_class_name('_2wP_Y'):
+        title = person.find_element_by_xpath('div/div/div[2]/div[1]/div[1]/span').text
+        # company = person.find_element_by_xpath('.//div[@class="company"]/a').text
+        if title in contactos:
+            i = 0
+        else:
+            contactos.append(title)
 
 
 def leerLoop():
     group_title = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div._2wP_Y")))
     for person1 in driver.find_elements_by_class_name('_2wP_Y'):
         person1.click()
-        group_title = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div._2wP_Y")))
-        for person in driver.find_elements_by_class_name('_2wP_Y'):
-            title = person.find_element_by_xpath('div/div/div[2]/div[1]/div[1]/span').text
-            # company = person.find_element_by_xpath('.//div[@class="company"]/a').text
-            if title in contactos:
-                pass
-            else:
-                contactos.append(title)
+        i = 0
+        i += 1
+        if i == 2:
+            group_title = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div._2wP_Y")))
+            for person in driver.find_elements_by_class_name('_2wP_Y'):
+                title = person.find_element_by_xpath('div/div/div[2]/div[1]/div[1]/span').text
+                # company = person.find_element_by_xpath('.//div[@class="company"]/a').text
+                if title in contactos:
+                    i = 0
+                else:
+                    contactos.append(title)
     return
 
 
-leerLoop()
+def ultimo(target):
+    x_arg = '//span[contains(@title,"' + target + '")]'
+    group_title = wait.until(EC.presence_of_element_located((
+        By.XPATH, x_arg)))
+    group_title.click()
+
+
+def leeer():
+    target = contactos[-1]
+    ultimo(target)
+    leer()
+
+
+leer()
 
 target = contactos[2]
 
-x_arg = '//span[contains(@title,"' + target + '")]'
-group_title = wait.until(EC.presence_of_element_located((
-    By.XPATH, x_arg)))
-group_title.click()
+contactoU = contactos[-1]
 
-time.sleep(5)
+ultimo(target)
 
-leerLoop()
+leer()
+
+contactoU2 = contactos[-1]
+
+while contactoU != contactoU2:
+    leeer()
+    contactoU = contactos[-1]
+    leeer()
+    contactoU2 = contactos[-1]
+
 
 for contactos1 in contactos:
     print(contactos1)
